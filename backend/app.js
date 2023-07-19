83a9cca474c7e3195b50cd06f2d266611e4c2b30
aisha0926 <aisha.0926@hotmail.com>
2023-07-19 19:29:26 +0800

    added boilerplate

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+await mongoose.connect(process.env.DB_CONNECTION, () => {
+  console.log('Database Connected');
+});
 
 app.listen(PORT, () => {
   console.log(`Listening in port ${PORT}`);
