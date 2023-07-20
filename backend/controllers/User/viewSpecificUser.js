3656dd554dcc2ea9f62c5b4800273b2b07136a96
aisha0926 <aisha.0926@hotmail.com>
2023-07-20 19:58:49 +0800

    get specific user

---
+++ backend/controllers/User/viewSpecificUser.js
@@ -0,0 +1,15 @@
+export default viewSpecificUser = async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const findUser = await User.findById({ _id: userId, status: 'active' });
+
+    findUser
+      ? res
+          .status(200)
+          .send({ message: 'Successfully found user', data: findUser })
+      : res.status(400).send({ message: 'Cannot find user' });
+  } catch (error) {
+    res.status(500).send({ message: 'Server error' });
+  }
+};
