e6f67e826d69f95c18cd794e3837b18f4b65930a
aisha0926 <aisha.0926@hotmail.com>
2023-07-20 19:50:08 +0800

    delete

--- backend/routes/dummy.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-import publicPosts from '../controllers/publicPosts.js';
-
-router.get('/route', publicPosts);
-
-export default router;
