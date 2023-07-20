1aa0f76f1a7e4e859a31461a8ae9496ee7e974f3
Mark Dj Junio <markdjjunio08@gmail.com>
2023-07-20 20:13:26 +0800

    feat: POST/reg
    added base logic for post/reg awaiting for jwt authentication

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import 'dotenv/config';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import User from './models/User.js';
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+app.use(express.json())
+app.use(cors());
+dotenv.config();
 
 mongoose
   .connect(process.env.DB_CONNECTION)
@@ -12,7 +17,23 @@ mongoose
   })
   .catch((err) => {
     console.log(err);
+});
+
+
+app.post('/register', async (req,res )=> {
+  const {firstName,lastName ,email  ,username, password,confirmpassword} = req.body;
+  const userDoc = await User.create({
+    firstName,
+    lastName,
+    email,
+    username,
+    password,
+    confirmpassword,
   });
+  res.json({userDoc});
+  
+
+})
 
 app.listen(PORT, () => {
   console.log(`Listening in port ${PORT}`);
