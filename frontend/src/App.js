51a022c3f7796230100b3d76982dd89584c9e240
Mark Dj Junio <markdjjunio08@gmail.com>
2023-07-21 01:08:46 +0800

    fix: added frontend
    * added frontend components to serve functions on be

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,33 @@
+import './App.css';
+import Post from './Routes/Post';
+import {Routes, Route} from 'react-router-dom'
+import Layout from './Routes/Layout';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+
+
+
 function App() {
-  return <h1>Test</h1>;
+  return (
+   
+      <Routes>
+       
+        <Route path="/" element={<Layout />}>
+        <Route index element={<Post />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+       
+        </Route>
+
+      
+      
+      </Routes>
+
+
+
+    
+    
+  );
 }
 
 export default App;
