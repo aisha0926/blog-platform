eae9cb5211e750fdbca1e8e7fb18800b7b759eef
Mark Dj Junio <markdjjunio08@gmail.com>
2023-07-20 21:32:56 +0800

    feat: post/register
    added hashing password

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,9 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import User from './models/User.js';
+import bcrypt from 'bcrypt';
+import { verifyUser,createToken } from './middlewares/auth.js';
+import jwt from 'jsonwebtoken'
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -19,7 +22,7 @@ mongoose
     console.log(err);
 });
 
-
+const salt = bcrypt.genSaltSync(10);
 app.post('/register', async (req,res )=> {
   const {firstName,lastName ,email  ,username, password,confirmpassword} = req.body;
   const userDoc = await User.create({
@@ -27,13 +30,13 @@ app.post('/register', async (req,res )=> {
     lastName,
     email,
     username,
-    password,
-    confirmpassword,
-  });
+    password:bcrypt.hashSync(password, salt),
+    confirmpassword});
   res.json({userDoc});
-  
 
 })
+
+
 
 app.listen(PORT, () => {
   console.log(`Listening in port ${PORT}`);
