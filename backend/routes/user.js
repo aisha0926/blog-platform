99dc1ef1ab1f397cae5c4aa0faafd035123582a0
aisha0926 <aisha.0926@hotmail.com>
2023-07-22 22:21:21 +0800

    feat: viewMe

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { loginUser } from '../controllers/loginUser.js';
 import { viewUserAccount } from '../controllers/viewUserAccount.js';
+import { verifyUser } from '../middlewares/auth.js';
+import { viewMe } from '../controllers/viewMe.js';
 
 const router = express.Router();
 
 router.post('/login', loginUser);
-
+router.get('/me', verifyUser, viewMe);
 router.get('/:userId', viewUserAccount);
 
 export default router;
