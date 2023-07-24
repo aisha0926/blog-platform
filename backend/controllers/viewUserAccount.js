252eeb9ac3ea94ec05d9bf762b77f86da0707b13
aisha0926 <aisha.0926@hotmail.com>
2023-07-24 22:13:51 +0800

    fix: inactive user

--- backend/controllers/viewUserAccount.js
+++ backend/controllers/viewUserAccount.js
@@ -5,10 +5,15 @@ export const viewUserAccount = async (req, res) => {
     const findUser = await User.findById(req.params.userId).select(
       '-password -_id -status -username -email'
     );
+
     //   If I'm logged in, I should be able to view my account
     if (!findUser) return res.status(404).send({ message: 'User not found' });
 
-    res.status(200).send({ message: 'Account found', data: findUser });
+    if (findUser.status === 'active') {
+      return res.status(200).send({ message: 'Account found', data: findUser });
+    }
+
+    res.status(400).send({ message: 'Account is inactive' });
   } catch (error) {
     res.status(500).send({ message: 'Server error', error: error.message });
   }
