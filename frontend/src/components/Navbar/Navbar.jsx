5a03a4dff55a7f66a155e1d6982aaa72247626a6
aisha0926 <aisha.0926@hotmail.com>
2023-07-26 19:43:36 +0800

    feat: navbar initial commit

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { FaHamburger } from 'react-icons/fa';
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div id='brand'>
+        <FaHamburger />
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
