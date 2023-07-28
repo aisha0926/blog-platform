f1a02f29ccdd3050a77b3330f68d4642a642082c
aisha0926 <aisha.0926@hotmail.com>
2023-07-28 17:37:58 +0800

    delete

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,20 +3,22 @@ import { FaHamburger } from 'react-icons/fa';
 
 function Navbar() {
   return (
-    <nav className='flex'>
-      {/* <div id='brand' className='flex-1 w-64'>
-        <FaHamburger />
-      </div>
-      <div id='brand' className='flex-1 w-32'>
-      <input type='text' /> 
-        <p>Test</p>
-      </div> */}
-
-      <div className='flex-none w-14 h-14'>01</div>
-      <div className='flex-initial w-64 ...'>02</div>
-      <div className='flex-initial w-32 ...'>03</div>
-    </nav>
+    <div class='flex'>
+      <div class='flex-none w-14 h-14'>01</div>
+      <div class='flex-initial w-64 ...'>02</div>
+      <div class='flex-initial w-32 ...'>03</div>
+    </div>
   );
 }
 
 export default Navbar;
+
+{
+  /* <div id='brand' className='flex-1 w-64'>
+  <FaHamburger />
+</div>
+<div id='brand' className='flex-1 w-32'>
+<input type='text' /> 
+  <p>Test</p>
+</div> */
+}
