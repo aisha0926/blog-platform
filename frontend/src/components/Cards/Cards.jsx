cc75fb29d2da3cbf74763444841b00a21e0d34b5
aisha0926 <aisha.0926@hotmail.com>
2023-07-28 17:51:57 +0800

    feat:cards

--- frontend/src/components/Cards/Cards.jsx
+++ frontend/src/components/Cards/Cards.jsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import { FaHamburger } from 'react-icons/fa';
 
 function Navbar() {
   return (
-    <div class='flex'>
-      <div class='flex-none w-14 h-14'>01</div>
-      <div class='flex-initial w-64 ...'>02</div>
-      <div class='flex-initial w-32 ...'>03</div>
+    <div class='flex flex-wrap'>
+      <div class='flex-none md:flex-1  w-screen bg-slate-100'>01</div>
     </div>
   );
 }
 
 export default Navbar;
-
-{
-  /* <div id='brand' className='flex-1 w-64'>
-  <FaHamburger />
-</div>
-<div id='brand' className='flex-1 w-32'>
-<input type='text' /> 
-  <p>Test</p>
-</div> */
-}
