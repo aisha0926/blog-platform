f974d5a45e5342bddd3b7ad1c1469519b407bb30
Grace Sio <grace.sio017@gmail,com>
2023-07-28 21:42:33 +0800

    navbar code

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
+import { Tabs, Tab, Box } from "@mui/material";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  const [value, setValue] = useState("Relevant");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        textColor="secondary"
+        indicatorColor="secondary"
+        aria-label="secondary tabs example"
+      >
+        <Tab value="Relevant" label="Relevant" />
+        <Tab value="Latest" label="Latest" />
+        <Tab value="Top" label="Top" />
+      </Tabs>
+    </Box>
+  );
 }
 
 export default Navbar;
