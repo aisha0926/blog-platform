881ef700152b759b24d0012e546c16224ca1a123
aisha0926 <aisha.0926@hotmail.com>
2023-07-28 17:39:39 +0800

    initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
-import Navbar from './components/Navbar/Navbar';
+import Cards from './components/Cards/Cards';
 
 function App() {
   return (
     <>
-      <Navbar />
+      <Cards />
     </>
   );
 }
