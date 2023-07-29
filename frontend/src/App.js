46fa6ab92aa4790c80f5bef3ec2c33a00b005643
Mark Dj Junio <markdjjunio08@gmail.com>
2023-07-29 12:04:19 +0800

    feat: added frontend create post logic
    *includes text editor
    *can upload image and post cover image.
    *include .css file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,13 @@
 import * as React from 'react';
-import LoginPage from './Pages/LoginPage.jsx';
-import RegisterPage from './Pages/RegisterPage.jsx'
 import { BrowserRouter as Router,Routes,  Route } from 'react-router-dom';
 import CreatePostPage from './Pages/CreatePostPage.jsx';
-import EditPostPage from './Pages/EditPostPage.jsx';
+
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
         <Route path="/createpost" element={<CreatePostPage />} />
-        <Route path="/editpost" element={<EditPostPage />} />
-
       </Routes>
       
     </Router>
