433232832caf77f1fa274a9ea46ad9c3daed7909
aisha0926 <aisha.0926@hotmail.com>
2023-07-29 17:37:54 +0800

    feat: aside functionality done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,18 @@
+import { useState } from 'react';
+import Aside from './components/Aside/Aside';
+
 function App() {
-  return <h1>Test</h1>;
+  const [showAside, setShowAside] = useState(false);
+
+  const asideBtn = () => {
+    setShowAside(!showAside);
+  };
+  return (
+    <>
+      <button onClick={asideBtn}>Click</button>
+      <Aside showAside={showAside} asideBtn={asideBtn} />
+    </>
+  );
 }
 
 export default App;
--- frontend/src/components/Aside/Aside.jsx
+++ frontend/src/components/Aside/Aside.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import styles from './Aside.module.css';
 
-function Aside() {
-  return <div>Aside</div>;
+function Aside({ showAside, asideBtn }) {
+  const toggleAside = () => {
+    asideBtn();
+  };
+  return (
+    <aside
+      className={showAside ? `${styles.show} ${styles.active}` : styles.hide}
+    >
+      <div className={styles['aside-header']}>
+        <h1>DEV Community</h1>
+        <button className={styles['aside-btn']} onClick={toggleAside}>
+          ❌
+        </button>
+      </div>
+
+      <ul>
+        <li>Home</li>
+        <li>Listings</li>
+        <li>Tags</li>
+      </ul>
+    </aside>
+  );
 }
 
 export default Aside;
