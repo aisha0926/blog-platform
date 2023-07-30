56703932a3738758121f0e0da1d2a981d348422a
Grace Sio <grace.sio017@gmail,com>
2023-07-30 16:06:23 +0800

    routes - general

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,16 @@
-function App() {
-  return <h1>Test</h1>;
-}
+import React, { useState } from "react";
+import AppRouter from "./Routes/AppRouter";
+
+const App = () => {
+  // need  logic to handle login state
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  return (
+    <div className="app">
+      {/* render the AppRouter component and pass the loggedIn state and setLoggedIn function as props */}
+      <AppRouter loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+    </div>
+  );
+};
 
 export default App;
