bfb89bfb13f78f3ec7d7475e7d06b13deb7c4466
Grace Sio <grace.sio017@gmail,com>
2023-07-30 10:43:14 +0800

    added fetch in the UserProfile (for now)

--- frontend/src/Pages/UserProfile.jsx
+++ frontend/src/Pages/UserProfile.jsx
@@ -5,41 +5,51 @@ import { Container, Stack } from "@mui/system";
 import AvatarImage from "../components/Avatar/AvatarImage";
 import { RiCake2Fill } from "react-icons/ri";
 
-function UserProfile() {
-  const profileId = "userId23";
+function UserProfile({ profileId, loggedInUsernme }) {
   const [isCurrentUser, setIsCurrentUser] = useState(false);
   const [profileData, setProfileData] = useState({});
 
   // Fetch profile data from the backend based on the profileId
+
   useEffect(() => {
     // Make an API call to your backend to fetch the user profile data using profileId
     // Set the fetched data in the state variable (profileData)
-    // Example:
-    // fetch(`/api/users/${profileId}`)
-    //   .then((response) => response.json())
-    //   .then((data) => setProfileData(data))
-    //   .catch((error) => console.log("Error fetching profile data: ", error));
-
-    // example, dummy data
-    setProfileData({
-      userId: "userId123",
-      username: "JohnDoe",
-      lastName: "Doe",
-      firstName: "John",
-      bio: "Web Developer ",
-      createdDate: "2023-07-28",
-      avatar: "https://one1onehomeschooling.co.uk/images/female-avatar.jpg",
-    });
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:4000/api/v1/user/${profileId}`
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP status ${response.status}`);
+        }
+        const user = await response.json();
+        setProfileData(user.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchUserData().finally(() => {});
+    console.log(profileData);
   }, [profileId]);
 
   useEffect(() => {
-    // Get the current logged-in user ID from your authentication state (e.g., JWT, local storage, etc.)
+    // Get the current logged-in user ID/username from your authentication state (e.g., JWT, local storage, etc.)
     // Set the isCurrentUser state variable accordingly
 
-    // For the purpose of this example, let's assume the logged-in user's ID is "userId123"
-    const loggedInUserId = "userId13";
-    setIsCurrentUser(loggedInUserId === profileData.userId);
+    setIsCurrentUser(loggedInUsernme === profileData.username);
   }, [profileData]);
+
+  //Format the date
+  const formatDate = (dateString) => {
+    try {
+      const date = new Date(dateString);
+      const options = { year: "numeric", month: "long", day: "numeric" };
+      return new Intl.DateTimeFormat("en-US", options).format(date);
+    } catch (error) {
+      console.error("Error parsing date:", error);
+      return "Invalid Date"; // Handle error
+    }
+  };
 
   return (
     <Container maxWidth="lg">
@@ -73,23 +83,25 @@ function UserProfile() {
         )}
 
         <Stack direction="column" spacing={1}>
-          <AvatarImage
-            height={150}
-            userData={profileData}
-            sx={{ border: "2px solid #ccc" }}
-          />
-          <Typography variant="username">{profileData.username}</Typography>
-
-          <Typography variant="subtitle1">
-            <RiCake2Fill /> <span />
-            Member since {profileData.createdDate}
-          </Typography>
-
-          <Typography variant="body2">{profileData.bio}</Typography>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <AvatarImage height={150} userData={profileData} hasBorder={true} />
+            <Typography variant="username">{profileData.username}</Typography>
+            <Typography variant="body2">
+              <RiCake2Fill /> <span />
+              Member since {formatDate(profileData.createdAt)}
+            </Typography>
+            <Typography variant="subtitle1">{profileData.bio}</Typography>
+          </Box>
         </Stack>
       </Box>
     </Container>
   );
 }
-
+// to call <UserProfile profileId={profileId} loggedInUsernme={loggedInUsernme} />
 export default UserProfile;
