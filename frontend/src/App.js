6aac2d0d72ac9ddbebd5c10e1f61716c06ddcad0
Grace Sio <grace.sio017@gmail,com>
2023-07-30 21:16:27 +0800

    scrap router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import Aside from "./components/Aside/Aside";
-import AppRouter from "./Routes/AppRouter";
 import "./App.css";
 
 function App() {
@@ -9,21 +8,13 @@ function App() {
   const asideBtn = () => {
     setShowAside(!showAside);
   };
-  // need  logic to handle login state
-  const [loggedIn, setLoggedIn] = useState(false);
 
   return (
     <>
-      <>
-        <button className="aside-toggle" onClick={asideBtn}>
-          Click
-        </button>
-        <Aside showAside={showAside} asideBtn={asideBtn} />
-      </>
-      <div className="app">
-        {/* render the AppRouter component and pass the loggedIn state and setLoggedIn function as props */}
-        <AppRouter loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
-      </div>
+      <button className="aside-toggle" onClick={asideBtn}>
+        Click
+      </button>
+      <Aside showAside={showAside} asideBtn={asideBtn} />
     </>
   );
 }
