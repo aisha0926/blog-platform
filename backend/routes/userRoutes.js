945803d4d601afd92dad555fa5de0f9608bf3fe8
aisha0926 <aisha.0926@hotmail.com>
2023-07-30 23:19:19 +0800

    cleaned backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,7 +6,7 @@ import { verifyUser } from '../middlewares/auth.js';
 import { viewMe } from '../controllers/User/viewMe.js';
 import { registerUser } from '../controllers/auth/registerUser.js';
 import updateUserProfile from '../controllers/User/updateUserProfile.js';
-import deactivateUser from '../controllers/deactivateUser/deactivateUser.js';
+import deactivateUser from '../controllers/User/deactivateUser.js';
 
 const router = express.Router();
 
