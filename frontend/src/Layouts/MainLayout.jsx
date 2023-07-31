6bfa7ac1f3d1612e4b8ae029cc9fdf66c80bccc1
aisha0926 <aisha.0926@hotmail.com>
2023-07-31 18:42:40 +0800

    feat-individual-post - deleted responsive

--- frontend/src/Layouts/MainLayout.jsx
+++ frontend/src/Layouts/MainLayout.jsx
@@ -1,26 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Aside from '../components/Aside/Aside';
 import styles from './MainLayout.module.css';
 
 function MainLayout() {
-  const [showAside, setShowAside] = useState(false);
-
-  const asideBtn = () => {
-    setShowAside(!showAside);
-  };
-
   return (
     <div className={styles['layout-container']}>
-      <button className='aside-toggle' onClick={asideBtn}>
-        Click
-      </button>
-
-      <Aside
-        className={styles['layout-container--aside']}
-        showAside={showAside}
-        asideBtn={asideBtn}
-      />
+      <Aside className={styles['layout-container--aside']} />
 
       <main className={styles['layout-container--main']}>
         <Outlet />
