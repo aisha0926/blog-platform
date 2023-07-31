5bf9359b2af3697a05cdc1bb2fb52cc221ebd1c6
aisha0926 <aisha.0926@hotmail.com>
2023-07-31 15:35:39 +0800

    feat-auth: login added status

--- backend/controllers/auth/loginUser.js
+++ backend/controllers/auth/loginUser.js
@@ -23,7 +23,12 @@ export const loginUser = async (req, res, next) => {
   try {
     const { username, password } = req.body;
 
-    const findUser = await User.findOne({ username: username });
+    const findUser = await User.findOne({
+      username: username,
+      status: 'active',
+    });
+
+    console.log(findUser);
 
     if (!findUser)
       return res.status(400).send({ message: `Cannot find user ${username}` });
