2f73486441ca38e653b08e99ec99f418c10d4e4d
jaydeej251 <markdjjunio08@gmail.com>
2023-08-09 22:04:30 +0800

    fix: added navigation for "dont have an account. sign up"

--- frontend/src/Pages/LoginPage.jsx
+++ frontend/src/Pages/LoginPage.jsx
@@ -204,7 +204,7 @@ function LoginPage() {
                   </Link>
                 </Grid>
                 <Grid item>
-                  <Link href="#" variant="body2">
+                  <Link href="/register" variant="body2">
                     {"Don't have an account? Sign Up"}
                   </Link>
                 </Grid>
